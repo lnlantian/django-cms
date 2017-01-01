4fc2de7f4bb79376420d864034612d8067288dcd
Lantianor <lnlantian@users.noreply.github.com>
2017-01-01 11:05:21 +0800

    Update models.py

--- menus/models.py
+++ menus/models.py
@@ -21,20 +21,12 @@ class CacheKeyManager(models.Manager):
         try:
             return super(CacheKeyManager, self).get_or_create(**kwargs)
         except CacheKey.MultipleObjectsReturned:
-            # Truncate the table, we don't want a funny cache object to cause
-            # mayhem!
+           
             CacheKey.objects.all().delete()
             return super(CacheKeyManager, self).get_or_create(**kwargs)
 
-
 class CacheKey(models.Model):
-    '''
-    This is to store a "set" of cache keys in a fashion where it's accessible
-    by multiple processes / machines.
-    Multiple Django instances will then share the keys.
-    This allows for selective invalidation of the menu trees (per site, per
-    language), in the cache.
-    '''
+
     language = models.CharField(max_length=255)
     site = models.PositiveIntegerField()
     key = models.CharField(max_length=255)
