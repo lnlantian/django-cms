bdb090a17bb94ebbab050188d3099ce39e113162
Lantianor <lantianor@126.com>
2017-02-08 09:27:23 +0800

    Update install-npm-dependencies.sh

--- scripts/install-npm-dependencies.sh
+++ scripts/install-npm-dependencies.sh
@@ -4,7 +4,6 @@ set -e
 
 SHRINKWRAP_FILE=npm-shrinkwrap.json
 SHRINKWRAP_CACHED_FILE=node_modules/npm-shrinkwrap.cached.json
-
 if diff -q $SHRINKWRAP_FILE $SHRINKWRAP_CACHED_FILE; then
   echo 'No shrinkwrap changes detected. npm install will be skipped...';
 else
